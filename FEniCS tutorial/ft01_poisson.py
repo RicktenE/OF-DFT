5298304996b925c67c2fd704d7b5ec14ee850f06
Rick <r.teneikelder@gmail.com>
2020-02-27 16:25:37 +0100

    poisson equations with multiple Dirichlet boundary conditions on x and Neumann on y

--- FEniCS tutorial/ft01_poisson.py
+++ FEniCS tutorial/ft01_poisson.py
@@ -10,33 +10,50 @@ Test problem is chosen to give an exact solution at all nodes of the mesh.
 """
 
 from __future__ import print_function
-from fenics import *
-from mpl_toolkits import mplot3d
-
-
+from fenics import UnitSquareMesh,plot, FunctionSpace, near, Function, errornorm, Expression, DirichletBC, TrialFunction, TestFunction, dot, grad, Constant, dx, ds, solve, File 
 import matplotlib.pyplot as plt
+
+
 # Create mesh and define function space
 mesh = UnitSquareMesh(8, 8)
 V = FunctionSpace(mesh, 'Lagrange', 1)
 
 # Define boundary condition
-u_D = Expression('1 + x[0]*x[0] + 2*x[1]*x[1]', degree=2)
+#u_D = Expression('1 + x[0]*x[0] + 2*x[1]*x[1]', degree=2)
 
-def boundary(x, on_boundary):
-    return on_boundary
+tol = 1E-14
 
-bc = DirichletBC(V, u_D, boundary)
+#Defining the left boundary
+def boundary_L(x, on_boundary):
+    return on_boundary and near(x[0], 0, tol)
+
+#Defining the right boundary
+def boundary_R(x, on_boundary):
+    return on_boundary and near(x[0], 1, tol)
+
+#defining expression on left boundary
+u_L = Expression('1 + 2*x[1]*x[1]', degree=2) 
+#Define left boundary
+bc_L = DirichletBC(V, u_L, boundary_L) 
+#defining expression on right boundary
+u_R = Expression('2 + 2*x[1]*x[1]', degree=2) 
+#Define right boundary
+bc_R = DirichletBC(V, u_R, boundary_R) 
+
+#collecting the left and right boundary in a list for the solver to read
+bcs = [bc_L, bc_R] 
 
 # Define variational problem
 u = TrialFunction(V)
 v = TestFunction(V)
 f = Constant(-6)
 a = dot(grad(u), grad(v))*dx
-L = f*v*dx
+g = Expression('-4*x[1]', degree=1) 
+L = f*v*dx - g*v*ds
 
 # Compute solution
 u = Function(V)
-solve(a == L, u, bc)
+solve(a == L, u, bcs)
 
 # Plot solution and mesh
 #plt.figure()
@@ -50,10 +67,10 @@ vtkfile = File('poisson/solution.pvd')
 vtkfile << u
 
 # Compute error in L2 norm
-error_L2 = errornorm(u_D, u, 'L2')
+error_L2 = errornorm(u_L, u, 'L2')
 
 # Compute maximum error at vertices
-vertex_values_u_D = u_D.compute_vertex_values(mesh)
+vertex_values_u_D = u_L.compute_vertex_values(mesh)
 vertex_values_u = u.compute_vertex_values(mesh)
 import numpy as np
 error_max = np.max(np.abs(vertex_values_u_D - vertex_values_u))
@@ -62,9 +79,7 @@ error_max = np.max(np.abs(vertex_values_u_D - vertex_values_u))
 print('error_L2  =', error_L2)
 print('error_max =', error_max)
 
-# plot
-# ax= plt.axes(projection="3d")
-plt.interactive(b)
+
 plt.show()
 
   
