d98f99fd8ede9ddb33c840014080aa42fa5e8618
Rick ten Eikelder <r.teneikelder@gmail.com>
2020-04-24 14:35:36 +0200

    clean up of repository

--- 0_Intern_project/Radial_solver_.py
+++ 0_Intern_project/Radial_solver_.py
@@ -1,10 +1,7 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
 ----------------------------------------------------------------------------------------------
 ----------------------------------------------------------------------------------------------
 Created on Mon Mar  2 09:27:19 2020
-
 @author: H.R.A. ten Eikelder
 program: OF-DFT radial atom solver for the TF equations.
 Description: This program takes as input the TF equation and gives 
@@ -21,7 +18,6 @@ import numpy as np
 from dolfin import *
 import matplotlib as mpl
 from matplotlib import pyplot as plt
-from petsc4py import *
 
 plt.close('all')
 
@@ -31,52 +27,41 @@ plt.close('all')
 ----------------------------------------------------------------------------------------------
 -------------------------------------------------------------------------------------------"""
 
+def plotting_solve_result_normal(u,title):
+    r = (mesh.coordinates())
+    x = np.sqrt(r)
+    y = [v*sqrt(u(v)) for v in r]
+    
+    #x = r
+    #y = [u(v) for v in r]
+    
+    plt.figure()
+    plt.title(title)
+    plt.xlabel("Radial coordinate")
+    plt.ylabel(title)
+    plt.grid()
+    #plt.plot(x,y)
+    plot(u)
+    # show the plots
+    plt.show()
+    return 
 
-def plotting_solve_result(u, mesh_bool):
-    if u == v_h :
-        plt.figure()
-        plt.title("Last calculated Internal potential v_h")
-        plt.xlabel("Radial coordinate")
-        #mpl.scale.LogScale(r)
-        plt.ylabel("Internal potential Vi")
-        plt.grid()
-        if mesh_bool == True :
-            plot(mesh)
-        plot(v_h)
-        
-    elif u == u_n :
-        #plt.figure()
-        plt.title("Last calculated density u_n")
-        plt.xlabel("Radial coordinate")
-        #a.set_xscale('squareroot')
-        #mpl.scale.LogScale(r)
-        plt.ylabel("Density [n]")
-        plt.grid()
-        if mesh_bool == True :
-            plot(mesh)
-        plot(u_n)
-        
-    elif u == nr :
-        plt.figure()
-        plt.title("nr")
-        plt.xlabel("Radial coordinate")
-        plt.ylabel("projection of Z/(4.0*pi)*gr.dx(0)/r ")
-        plt.grid()
-        if mesh_bool == True :
-            plot(mesh)
-        plot(nr)
-    elif u == gr:
-         plt.figure(1)
-         plt.title("gr")
-         plt.xlabel("Radial coordinate")
-         plt.ylabel("projection of derivative of n towards r")
-         plt.grid()
-         if mesh_bool == True :
-            plot(mesh)
-         plot(gr)
-    else :
-        print("The input to plot is invalid")
-        
+
+def plotting_solve_result(u,title):
+    r = (mesh.coordinates())
+    x = np.sqrt(r)
+    y = [v*sqrt(u(v)) for v in r]
+    
+    #x = r
+    #y = [u(v) for v in r]
+    
+    plt.figure()
+    plt.title(title)
+    plt.xlabel("Radial coordinate")
+    plt.ylabel(title)
+    plt.grid()
+    plt.plot(x,y)
+    #plot(u)
     # show the plots
     plt.show()
     return 
@@ -185,7 +170,7 @@ class Weizsacker(object):
 
 func_weizsacker = Weizsacker()
 
-functionals = [#TF(),\
+functionals = [TF(),\
                Dirac(),\
                Weizsacker()\
                ]      
@@ -195,10 +180,12 @@ functionals = [#TF(),\
 ----------------------------------------------------------------------------------------------
 -------------------------------------------------------------------------------------------""" 
 # Create mesh and define function space
-start_x = 0.1
-end_x = 20
+start_x = 0.5
+end_x = 6
 amount_vertices = 200
 mesh = IntervalMesh(amount_vertices,start_x, end_x) # Splits up the interval [0,1] in (n) elements 
+
+print("MES COORDS",np.shape(mesh.coordinates()))
 
 #Creation of Function Space
 P1 = FiniteElement("P", mesh.ufl_cell(), 2)
@@ -210,13 +197,17 @@ W = FunctionSpace(mesh, element)
 #Define radial coordinates based on mesh
 r = SpatialCoordinate(mesh)[0] # r are the x coordinates. 
 
-#Element Kr
-#Z = Constant(36) # Krypton
-#N = Z # Neutral 
-
 #Element H
-Z = Constant(1) # Hydrogen
-N = Z # Neutral
+#Z = Constant(1) # Hydrogen
+#N = Z # Neutral
+
+#Element Ne
+#Z = Constant(10) # Neon
+#N = Z # Neutral
+
+#Element Kr
+Z = Constant(36) # Krypton
+N = Z # Neutral 
 
 
 """-------------------------------------------------------------------------------------------
@@ -224,30 +215,6 @@ N = Z # Neutral
                     Creating and defining the boundary conditions
 ----------------------------------------------------------------------------------------------
 -------------------------------------------------------------------------------------------"""
-
-
-"""def set_boundary(start_x, end_x,tol, left_value, right_value, V):
-    #Defining the tolerance on the boundaries 
-    tol = 1E-14
-
-    #Defining the left boundary
-    def boundary_L(x, on_boundary):
-        return on_boundary and near(x[0], start_x, tol)
-
-    #Defining the right boundary
-    def boundary_R(x, on_boundary):
-        return on_boundary and near(x[0], end_x, tol)
-
-    #Defining expression on left boundary
-    n_L = Expression(left_value, degree=1)         
-    bc_L = DirichletBC(V, n_L, boundary_L)  
-    
-    #Defining expression on right boundary
-    n_R = Expression(right_value, degree=1)
-    bc_R = DirichletBC(V, n_R, boundary_R) 
-    
-    #collecting the left and right boundary in a list for the solver to read
-    bcs = [bc_L, bc_R] """
 
 #Defining the tolerance on the boundaries 
 tol = 1E-14
@@ -275,41 +242,34 @@ bcs = [bc_L, bc_R]
                     Defning external potential v[r] and Initial density n_1[r]
 ----------------------------------------------------------------------------------------------
 -------------------------------------------------------------------------------------------"""
-#External potential v_e[r] is analytically described for atoms 
-Ex = -Z/r
-#Ex =Constant(1)
-
-"""
-#Fake External potential
-Ex = Function(V)
-rhs = Function(V)
-
-Ex.vector()[:] =0
-rhs.vector()[:]  = 0 
-u = TrialFunction(V)
-v = TestFunction(V)
-a = dot(grad(u), grad(v))*dx
-L = rhs*v*dx
-A,b = assemble_system(a, L, bcs)
-
-solve(A, Ex.vector(), b)
-
-print('printing Ex', Ex.vector().get_local())
-"""
-            
-
 #---constants---
 lamb = 1/9
 C1 = 3/10*(3*math.pi**2)**(2/3)
 C2 = 3/4*(3/math.pi)**(1/3)
 C3 = lamb/8
-mu = 0
+mu = 1
 omega = Constant(1)
 
-#------ Initial density ------]
+A_ = 4*C3
+B_ = (-5/3)*C1
+D_ = 1#(4/3)*C2
 
-#n_i = Expression('a*exp(-b*pow(x[0], 2))', degree = 2, a = 1/sqrt(2*pi), b=1)
-n_i = Constant(1)
+A__ = (5/3)*(C1/C3)
+B__ = 4/3
+C__ =  1/C3
+
+#----- External Potential ----
+
+#External potential v_e[r] is analytically described for atoms 
+Ex = -Z/r
+
+#------ Initial density ------
+
+n_i = Expression('a*exp(-b*pow(x[0], 2))', degree = 2, a = 1, b=1.0)
+#n_i = Expression('pow(x[0],2)', degree = 2)
+#n_i = Constant(1)
+
+
 nlast = Function(V)
 
 #------------Checking amount of electrons ----------------------
@@ -329,33 +289,29 @@ print("Number of electrons after adjustment:",intn)
 v_h = interpolate(Constant(-1), V)
 
 mixed_test_functions = TestFunction(W)
-(vr, pr) = split(mixed_test_functions)
+(qr, pr) = split(mixed_test_functions)
 
 du_trial = TrialFunction(W)        
 du = Function(W)
 
 # Put the initial density and v_h in u_k
+
 u_k = Function(W)
 assign(u_k.sub(0), v_h)
 assign(u_k.sub(1), u_n)
 
 
 bcs_du = []
+
 eps = 1
 iters = 0
-maxiter = 1000
+maxiter = 200
 minimal_error = 1E-9
 
 while eps > minimal_error and iters < maxiter:
     iters += 1 
     
-    #######print('check begin loop', iters)
     (v_hk, u_nk) = split(u_k)
-    
-    #---- Rewriting variables for overview---------
-    l = sqrt(r)
-    y = r*sqrt(u_nk)
-    Q = r*(Ex+v_hk)
     
     #---- Setting up functionals -------------------
     densobj = DensityRadialWeakForm(u_nk, pr)
@@ -367,31 +323,49 @@ while eps > minimal_error and iters < maxiter:
             funcpots += f.potential_weakform(densobj)
             
     #---- Solving v_h and u_n ----------------------
+   
+    ## Variational equation With Radial transformation 
+    #--- Rewriting variables for overview ---
+    U_ = sqrt(u_nk)
+    E_ = (-Ex - v_hk - mu)
     
-    #First coupled equation: Q'' = 1/l*Q' +16pi*y^2
-    F = Q.dx(0)*vr.dx(0)*dx                                \
-    - 1/l*Q.dx(0)*vr.dx(0)*dx                               \
-    - 16*math.pi*y**2*vr*dx  
+    F = - A_*U_.dx(0)*qr.dx(0)*dx      \
+        + 2*A_*(1/r)*U_.dx(0)*qr*dx \
+        + B_*u_nk**(7/6)*qr*dx      \
+        + D_*u_nk**(5/6)*qr*dx      \
+        - Ex*U_*qr*dx              \
+        - v_hk*U_*qr*dx            \
+        - mu*U_*qr*dx
     
-    # Second coupled equation y'' = ... y ... x ... Q
-    F = F + y.dx(0)*pr.dx(0)*dx                             \
-    - y.dx(0)/l*pr*dx                                             \
-    - (5*C1)/(3*C3)*(y)**(7/3)/(l)**(2/3)*pr*dx             \
-    + 4/3*(l)**(2/3)*(y)**(5/3)*pr*dx                     \
-    - 1/C3*(mu*r-Q)*y*pr*dx  
+    #Poisson equation of Hatree potential
+    F = F -(                          \
+        - v_hk.dx(0)*pr.dx(0)*dx    \
+        + (2/r)*v_hk.dx(0)*pr*dx  \
+        + 4*math.pi*u_nk*pr*dx)
+    
     
     #Calculate Jacobian
     J = derivative(F, u_k, du_trial)
     
     #Assemble system
     A, b = assemble_system(J, -F, bcs_du)
+
+    #nvec = u_k.sub(1).vector()
+    rvec = mesh.coordinates()
+    nvec = np.array([v_hk(v) for v in rvec])
+    minval = nvec.min()
+    print("v_hk minimum:",minval)
+
+    rvec = mesh.coordinates()
+    nvec = np.array([u_nk(v) for v in rvec])
+    minval = nvec.min()
+    print("u_nk minimum:",minval)    
     
-    #print('du before solve', du.vector().get_local())
+    #plotting_solve_result(v_hk,"v_nk pre solver")
+    plotting_solve_result_normal(u_nk,"u_nk pre solver")
     
     solve(A, du.vector(), b)
-    
-    #print('du after solve', du.vector().get_local())
-        
+
     # Conserve memory by reusing vectors for v_h and u_n to keep dv_h and du_n
     dv_h = v_h                  #step to transfer type info to dv_h
     assign(dv_h, du.sub(0))     #step to transfer values to dv_h
@@ -400,21 +374,22 @@ while eps > minimal_error and iters < maxiter:
     du_n = u_n                  #step to transfer type info to du_n
     assign(du_n, du.sub(1))     #step to transfer values to du_n
     u_n = None                  #empty u_n
+
+    #plotting_solve_result(dv_h,"dv_h")
+    plotting_solve_result(du_n,"du_n") 
     
-    #print('check for dv_h ', dv_h.vector().get_local())
     #---- Calculate the Error -----------------------
-    #error_L2 = errornorm(du_n, dv_h, 'L2')
-    #eps = error_L2
-    avg = sum(dv_h.vector().get_local())/len(dv_h.vector().get_local())
+    avg = sum(du.vector().get_local())/len(du.vector().get_local())
     eps = np.linalg.norm(du.vector().get_local()-avg, ord=np.Inf)
     if math.isnan(eps):
             raise Exception("Residual error is NaN")
 
+    print("EPS is:",eps)
+        
+     #---- Taking the step for u_n and v_h ------------
     
-    #---- Taking the step for u_n and v_h ------------
-    assign(nlast, u_k.sub(1))
     u_k.vector()[:] = u_k.vector()[:] + omega*du.vector()[:]
-    
+        
     # Conserve memory by reusing vectors for u_n, v_h also as du_n, dv_h
     v_h = dv_h 
     assign(v_h, u_k.sub(0))
@@ -423,12 +398,15 @@ while eps > minimal_error and iters < maxiter:
     u_n = du_n 
     assign(u_n, u_k.sub(1))
     du_n = None  
-                      
+
+    plotting_solve_result(u_n,"density")
+    
     #---- Ad hoc negative density fix -------
-    #print('check for u_n before', u_n.vector().get_local())
+    #print('check for negative u_n before', u_n.vector().get_local())
     omega = 1 
     nvec = u_n.vector()
     minval = nvec.min()
+    print("Going to add:",minval+1)
     if minval <= 0.0:
         nvec[:]=nvec[:]-minval+1
         intn = float(assemble((u_n)*dx(mesh)))
@@ -437,20 +415,27 @@ while eps > minimal_error and iters < maxiter:
         intn = float(assemble((u_n)*dx(mesh)))            
         assign(u_k.sub(1),u_n)
         print("Number of electrons after correction:",intn)
-        
-    #print('check for negative u_n after', u_n.vector().get_local())
-   
-    #------- Calculate v_h allignment correction 
-    vh_align = float(assemble((v_h)*dx(mesh)))
-    mu_new = mu - vh_align
+
+    nvec = u_k.sub(1).vector()
+    minval = nvec.min()
+    print("Minval after correction:",minval)
     
-    v_h.vector()[:] += vh_align
-    vh_align = 0.0
     print('Check end of loop, iteration: ', iters,' Error: ', eps)
     
-plotting_solve_result(u_n, False)
-plotting_solve_result(v_h, False)
+    
+    #---- vh -> u_n
+    #u = TrialFunction(V)
+    #v = TestFunction(V)        
+    #a = u*v*dx
+    # Works but with oscillations
+    #L = (-1.0/(4.0*pi))*(2/r*v_h.dx(0)*v-v_h.dx(0)*v.dx(0))*dx
+    #solve(a == L, u_n ,bcs)
+    
+    #plotting_solve_result(u_n)
+    #plotting_solve_result(v_h)
+    
 
+"""
 #------------------------- calculate v_h
 calc_vh = Function(V)
 rhs = Function(V)
@@ -459,85 +444,16 @@ calc_vh.vector()[:]=0
     
 u = TrialFunction(V)
 v = TestFunction(V)
-a = dot(grad(u), grad(v))*dx
+a = u*v*dx
 L = rhs*v*dx
-
 A,b = assemble_system(a, L, bcs)
-
 solve(A, calc_vh.vector(), b)
 v_h = calc_vh
 #---------------------------------solve for u_n from v_h
-bsc = []
 a = u*v*dx
 L = (1.0/(4.0*pi))*inner(grad(v_h),grad(v))*dx
 solve(a == L, u_n,bcs)
-
-  
-plotting_solve_result(u_n, False)
-plotting_solve_result(v_h, False)
-
-
-
-
-
-
-
-
-"""
-Error as written in PyDeFuSe
-#avg = sum(dv_h.vector().get_local())/len(dv_h.vector().get_local())
-    #eps = np.linalg.norm(du.vector().get_local()-avg, ord=np.Inf)
-    
-     
-
-
-https://www.mcs.anl.gov/petsc/petsc-current/docs/manualpages/Vec/index.html 
-
-    #---- Try to get values with numpy.array() --------
-    #avg = sum(np.array(dv_h.vector())/len(np.array(dv_h.vector())))
-    #eps = np.linalg.norm(np.array(du.vector())-avg, ord=np.Inf)
-    #print('Iteration for self-consistency:', iters,'norm:', eps)
-    #if math.isnan(eps):
-    #        raise Exception("Residual error is NaN")    
-            
-    #---- Try to get values with gather() --------------
-    #array1 = gather(dv_h)
-    #array2 = gather(du)
-    #avg = sum(array1())/len(array1())
-    
-    #----- Try to get values with VecGetArrayRead() / VecGetValues() ---- 
-    #testarray = VecGetArrayRead(dv_h)
-    #avg = sum(testarray)/len(testarray)
-    #testarray2 = VecGetArrayRead(du_n)
-    #eps = np.linalg.norm(testarray2-avg, ord=np.Inf)
-
-    #----- As it is written in PyDeFuSe ---------------------- 
-    #avg = sum(dv_h.vector().array())/len(dv_h.vector().array())
-    #eps = numpy.linalg.norm(du_n.vector().array()-avg, ord=numpy.Inf)
-    
-    #--Negative density fix ' max step'      
-    #du_nvec = du_n.vector()
-    #if (du_nvec<0.0).sum() > 0:
-    #    maxomega = -min(np.divide(nlast.vector()[du_nvec<0],du_n.vector()[du_nvec<0]))
-    #    if maxomega > 0 and maxomega < omega:
-    #        omega = maxomega/2.0
-
-
-
-#-----------------mu_new will be used for Calculating the energies
-#gr = project(u_n.dx(0),V)
-#plotting_solve_result(gr, False)
-
-#nr = project(Z/(4.0*pi)*gr.dx(0)/r,V)
-#plotting_solve_result(nr, False) 
-
-"""
-"""-------------------------------------------------------------------------------------------
-----------------------------------------------------------------------------------------------
-                    Saving VTKfile for post processing in ParaView
-----------------------------------------------------------------------------------------------
--------------------------------------------------------------------------------------------
-#Save solution to file in VTK format
-vtkfile = File('VTKfiles/radial_atom_solver_TF.pvd')
-vtkfile << u_n
+#u_n = r*sqrt(u_n)
+plotting_solve_result(u_n)
+plotting_solve_result(v_h)
 """
